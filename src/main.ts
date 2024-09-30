3546c84bb0e46debe1b5416ed1f6121e23a90b9a
hzihhuang <hzih.huang@ecoflow.com>
2024-09-30 15:56:35 +0800

    perf: 允许跨域&静态资源访问

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  // 增加静态资源访问点(localhost:3000/static)，对应 public 目录
+  app.useStaticAssets('public', { prefix: '/static' });
+
+  // 允许跨域
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
